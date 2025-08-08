36f021355cd5143408b32f1106f0d883c6ee9a67
ebonknd <ebonknd@gmail.com>
2025-08-08 10:07:05 -0300

    Add files via upload

---
+++ pob1/src/br/com/ucb/main/main.java
@@ -0,0 +1,29 @@
+package br.com.ucb.main;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//comentario sobre o que esse metodo faz
+		
+		/*comentario multiplas linhas
+		 * oii
+		 */
+		
+		String nome = "julio";
+		int idade = 39;
+		
+		if (idade>39) {
+			System.out.println(idade + " ta ficando velho");
+		} 
+		
+		else {
+			System.out.println(idade + " ta novo");
+		}
+		
+		
+
+	}
+
+}
---
+++ sistema.ucb/src/br/com/ucb/main/Main.java
@@ -0,0 +1,26 @@
+package br.com.ucb.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String nome = "emi";
+		int nota = 6;
+		
+		if(nota >= 7) {
+			
+			System.out.println(nome+ " foi aprovada");
+		}
+		
+		else {
+			
+			System.out.println(nome+ " foi reprovada" );
+		}
+		
+		
+
+	}
+
+}
