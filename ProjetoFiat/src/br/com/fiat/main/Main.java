2300d4926806d0dd134d95196c42706a9a022c6f
ebonknd <ebonknd@gmail.com>
2025-08-15 10:35:26 -0300

    Add files via upload

---
+++ ProjetoFiat/src/br/com/fiat/main/Main.java
@@ -0,0 +1,16 @@
+package br.com.fiat.main;
+
+import br.com.fiat.veiculo.Carro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//vamos instanciar um objeto carro (tirando da forma)
+		
+		Carro objFerrari = new Carro("Ferrari", "488 spider", 2023-*);
+		objFerrari.acelerar();
+
+	}
+
+}
---
+++ ProjetoFiat/src/br/com/fiat/veiculo/Carro.java
@@ -0,0 +1,26 @@
+package br.com.fiat.veiculo;
+
+public class Carro {
+
+	//atributos - caracteristicas
+
+	private String marca;
+	private String modelo;
+	private int ano;
+
+	public Carro(String marca, String modelo, int ano) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.ano = ano;
+	}
+
+	public void acelerar() 
+	
+		System.out.println(modelo+ "Carro acelerando...Vrumm <3");
+	} 
+	,
+	,
+
+}
+
